8be26a99dd64b3d987d1807a4d3ac45330d25e90
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2024-11-15 15:42:22 +0700

    Setup project: bootstrap; navbar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Welcom to FPoly!!!</h1>
+  </main>
+</template>
---
+++ src/views/Students/View.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Students</h1>
+  </main>
+</template>
